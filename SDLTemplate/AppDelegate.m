4c0846113e12d61ca58f60a8684023211387fe3a
Steve Richey <srichey@floatlearning.com>
2014-12-09 14:48:06 -0600

    Initial Commit

---
+++ SDLTemplate/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  SDLTemplate
+//
+//  Created by Steve Richey on 12/9/14.
+//  Copyright (c) 2014 changeme. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ SDLTemplate/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SDLTemplate
+//
+//  Created by Steve Richey on 12/9/14.
+//  Copyright (c) 2014 changeme. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
